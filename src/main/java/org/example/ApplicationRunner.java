6b860c87c33487354d9fbbcb3ae66ad6566f5c31
kukuru5a <romanzag@icloud.com>
2024-05-20 14:53:17 +0600

    XML type BD

--- src/main/java/org/example/ApplicationRunner.java
+++ src/main/java/org/example/ApplicationRunner.java
@@ -5,20 +5,12 @@ import org.example.rzag.spring.database.repository.repository.CompanyRepository;
 import org.example.rzag.spring.database.repository.repository.UserRepository;
 import org.example.rzag.spring.ioc.Container;
 import org.example.rzag.spring.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
-        var contqainer = new Container();
-//        var connectionPool = new ConnectionPool();
-//        var userRepository = new UserRepository(connectionPool);
-//        var companyRepository = new CompanyRepository(connectionPool);
-//        var us = new UserService(userRepository, companyRepository);
-
-        var connectionPool = contqainer.getClass(ConnectionPool.class);
-        var userRepository = contqainer.getClass(UserRepository.class);
-        var companyRepository = contqainer.getClass(CompanyRepository.class);
-        var us = contqainer.getClass(UserService.class);
-
-
+    var context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        var connectionPool1 = context.getBean("pool2", ConnectionPool.class);
+        var connectionPool2 = context.getBean("pool1", ConnectionPool.class);
     }
 }
